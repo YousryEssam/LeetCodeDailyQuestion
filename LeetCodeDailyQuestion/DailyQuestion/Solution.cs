ca19257f7fe32c071befa2060cc5285a1408786c
YousryEssam <youssry.essam2111@gmail.com>
2025-03-30 13:55:48 +0200

    Miscellaneous problems : 2405. Optimal Partition of String

---
+++ LeetCodeDailyQuestion/DailyQuestion/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDailyQuestion.DailyQuestion
+{
+    // This Class have Miscellaneous problems 
+    class Solution
+    {
+        // 2405. Optimal Partition of String
+        public int PartitionString(string s)
+        {
+            int ans = 1;
+            int[] frq = new int[26];
+            for(int i =0; i < s.Length; i ++)
+            {
+                if(frq[s[i] - 'a'] > 0) 
+                {
+                    ans++;
+                    for (int j = 0; j < 26; j++)
+                    {
+                        frq[j] = 0;
+                    }
+                }
+                frq[s[i] - 'a']++;
+            }
+            return ans;
+        }
+    }
+}
