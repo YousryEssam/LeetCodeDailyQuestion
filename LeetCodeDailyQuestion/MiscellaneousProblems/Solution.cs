5a21243fbf884a36fa18192c185bdd1450cb62c1
YousryEssam <youssry.essam2111@gmail.com>
2025-03-31 20:48:02 +0200

    2240. Number of Ways to Buy Pens and Pencils

--- LeetCodeDailyQuestion/MiscellaneousProblems/Solution.cs
+++ LeetCodeDailyQuestion/MiscellaneousProblems/Solution.cs
@@ -9,6 +9,22 @@ namespace LeetCodeDailyQuestion.MiscellaneousProblems
     // This Class have Miscellaneous problems 
     class Solution
     {
+
+        //2240. Number of Ways to Buy Pens and Pencils 
+        public long WaysToBuyPensPencils(int total, int cost1, int cost2)
+        {
+            long answer = 0;
+            long curSum = 0;
+            if (cost1 < cost2)
+                (cost1, cost2) = (cost2, cost1);
+            while (curSum <= total)
+            {
+                long temp = 1 + ((total - curSum) / cost2);
+                answer += temp;
+                curSum += cost1;
+            }
+            return answer;
+        }
         // 2405. Optimal Partition of String
         public int PartitionString(string s)
         {
