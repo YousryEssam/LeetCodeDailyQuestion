5f334d5e8d268d2efd5bc7645a4d940065acff80
YousryEssam <youssry.essam2111@gmail.com>
2025-04-09 21:11:13 +0200

    3375. Minimum Operations to Make Array Values Equal to K

---
+++ LeetCodeDailyQuestion/DailyQuestionApril2025/P3375.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDailyQuestion.DailyQuestionApril2025
+{
+    class P3375
+    {
+        // 3375. Minimum Operations to Make Array Values Equal to K
+        public int MinOperations(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            int res = 0;
+            for (int i = 0; i < nums.Length; i++) {
+                if (nums[i] < k)
+                {
+                    return -1;
+                }
+                else if (nums[i] > k) {
+                    if (i == 0 || nums[i] != nums[i - 1]) { 
+                        res++;
+                    }
+
+                }
+            }
+            return res;
+        }
+    }
+}
