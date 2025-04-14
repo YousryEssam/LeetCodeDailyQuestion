35b02ac9a0e1491c8403934a17396972c3278fcc
YousryEssam <youssry.essam2111@gmail.com>
2025-04-14 23:35:42 +0200

    1534. Count Good Triplets

---
+++ LeetCodeDailyQuestion/DailyQuestionApril2025/P1534.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDailyQuestion.DailyQuestionApril2025
+{
+    class P1534
+    {
+        public int CountGoodTriplets(int[] arr, int a, int b, int c)
+        {
+            // 1534. Count Good Triplets
+            int result = 0, n = arr.Length;
+            for(int i = 0; i < n; i ++)
+            {
+                for(int j = i + 1 ; j < n; j++)
+                {
+                    for(int k =  j + 1 ; k < n; k++)
+                    {
+                        if( Math.Abs(arr[i] - arr[j]) <= a && 
+                            Math.Abs(arr[k] - arr[j]) <= b && 
+                            Math.Abs(arr[k] - arr[i]) <= c)
+                        {
+                            result++;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
