bb31efbbf4997156527a5d7c770a6de39a1602c3
YousryEssam <youssry.essam2111@gmail.com>
2025-04-17 19:56:35 +0200

    2006. Count Number of Pairs With Absolute Difference K

---
+++ LeetCodeDailyQuestion/MiscellaneousProblems/P2006.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDailyQuestion.MiscellaneousProblems
+{
+    internal class P2006
+    {
+        // 2006. Count Number of Pairs With Absolute Difference K
+        public int CountKDifference(int[] nums, int k)
+        {
+            int result = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (Math.Abs(nums[i] - nums[j]) == k)
+                    {
+                        result++;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
