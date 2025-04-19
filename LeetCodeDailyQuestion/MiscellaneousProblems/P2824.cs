4ff9f10e7e32f75ab2cd9cc81fe5020e0fe8b68f
YousryEssam <youssry.essam2111@gmail.com>
2025-04-19 16:39:14 +0200

    2824. Count Pairs Whose Sum is Less than Target

---
+++ LeetCodeDailyQuestion/MiscellaneousProblems/P2824.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDailyQuestion.MiscellaneousProblems
+{
+    internal class P2824
+    {
+        // 2824. Count Pairs Whose Sum is Less than Target
+        public int CountPairs(IList<int> nums, int target)
+        {
+            nums = nums.OrderBy(x => x).ToList();
+            int count = 0; 
+            int left = 0; 
+            int right = nums.Count - 1; 
+            while (left < right)
+            {
+                if (nums[left] + nums[right] < target)
+                { 
+                    count += right - left; 
+                    left++; 
+                }
+                else
+                { 
+                    right--;
+                }
+            }
+            return count;
+        }
+    }
+}
