b4dacb4c3ad33be87d5791da92ce7288a34b436d
YousryEssam <youssry.essam2111@gmail.com>
2025-04-25 23:00:35 +0300

    2845. Count of Interesting Subarrays

---
+++ LeetCodeDailyQuestion/DailyQuestionApril2025/P2845.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDailyQuestion.DailyQuestionApril2025
+{
+    internal class P2845
+    {
+        // 2845. Count of Interesting Subarrays
+        public long CountInterestingSubarrays(IList<int> nums, int modulo, int k)
+        {
+            var cnt = new Dictionary<int, int> { [0] = 1 };
+            long res = 0;
+            int prefix = 0;
+            foreach (var num in nums)
+            {
+                if (num % modulo == k) prefix++;
+                int target = (prefix - k + modulo) % modulo;
+                if (cnt.TryGetValue(target, out int val)) res += val;
+                cnt[prefix % modulo] = cnt.GetValueOrDefault(prefix % modulo, 0) + 1;
+            }
+            return res;
+        }
+    }
+}
