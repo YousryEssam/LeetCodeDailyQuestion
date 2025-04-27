9fc5b0b65e96c14a215eb7f8f7638a12785f266d
YousryEssam <youssry.essam2111@gmail.com>
2025-04-27 21:36:04 +0300

    3392. Count Subarrays of Length Three With a Condition

---
+++ LeetCodeDailyQuestion/DailyQuestionApril2025/P3392.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDailyQuestion.DailyQuestionApril2025
+{
+    internal class P3392
+    {
+        // 3392. Count Subarrays of Length Three With a Condition
+        public int CountSubarrays(int[] nums)
+        {
+            int counter = 0;
+            for (int i = 0; i < nums.Length -2; i++) {
+                if ((nums[i] + nums[i+2]) * 2 == nums[i + 1])
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
