35357e45a36974f78e90278a9687820299fcfe26
YousryEssam <youssry.essam2111@gmail.com>
2025-05-06 20:21:33 +0300

    Add P1920 class with BuildArray method
    Introduces a new class `P1920` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeFroMay2025`.
    The `BuildArray` method constructs a new array from
    the input permutation array `nums` by indexing into
    it, returning the resulting array.

---
+++ LeetCodeDailyQuestion/DailyChallengeFroMay2025/P1920.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDailyQuestion.DailyChallengeFroMay2025
+{
+    internal class P1920
+    {
+        // 1920. Build Array from Permutation
+        public int[] BuildArray(int[] nums)
+        {
+            int[] ans = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++) 
+            {
+                ans[i] = nums[nums[i]];
+            }
+            return ans;
+        }
+    }
+}
