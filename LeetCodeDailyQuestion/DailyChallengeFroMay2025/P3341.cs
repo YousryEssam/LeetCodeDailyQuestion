718ee1392f312a0df68b1e605b3e3f548003e60d
YousryEssam <youssry.essam2111@gmail.com>
2025-05-07 22:05:31 +0300

    Add P3341 class with MinTimeToReach method
    Introduces a new class `P3341` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeFroMay2025`.
    The `MinTimeToReach` method calculates the minimum time
    to reach the last room in a grid using a priority queue
    and a pathfinding algorithm. It initializes a 2D array
    to track minimum times and processes each cell until
    the last room is reached, returning the minimum time.

---
+++ LeetCodeDailyQuestion/DailyChallengeFroMay2025/P3341.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDailyQuestion.DailyChallengeFroMay2025
+{
+    internal class P3341
+    {
+        // 3341. Find Minimum Time to Reach Last Room I 
+        // Day 7 / 5 / 2025
+        public int MinTimeToReach(int[][] moveTime)
+        {
+
+            int n = moveTime.Length, m = moveTime[0].Length;
+            int[,] minTime = new int[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    minTime[i, j] = Int32.MaxValue;
+                }
+            }
+
+            PriorityQueue<(int, int), int> pq = new PriorityQueue<(int, int), int>();
+            pq.Enqueue((0, 0), 0);
+
+            int[,] dirs = new int[,] { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
+            while (pq.Count > 0)
+            {
+                pq.TryDequeue(out (int, int) curr, out int currTime);
+                for (int d = 0; d < 4; d++)
+                {
+                    int newRow = curr.Item1 + dirs[d, 0];
+                    int newCol = curr.Item2 + dirs[d, 1];
+                    if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m)
+                    {
+                        int newTime = Math.Max(currTime, moveTime[newRow][newCol]) + 1;
+                        if (newTime < minTime[newRow, newCol])
+                        {
+                            minTime[newRow, newCol] = newTime;
+                            pq.Enqueue((newRow, newCol), newTime);
+                        }
+                    }
+                }
+            }
+
+            return minTime[n - 1, m - 1];
+        }
+    }
+}
