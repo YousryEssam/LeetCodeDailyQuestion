45450bc9158d3af3de71b9837dc17d737b7e1dba
YousryEssam <youssry.essam2111@gmail.com>
2025-05-10 21:10:30 +0300

    Add P1360 class for calculating days between dates
    Introduced a new class `P1360` in the namespace
    `LeetCodeDailyQuestion.MiscellaneousProblems`. This class
    features the method `DaysBetweenDates`, which computes
    the absolute number of days between two date strings
    using `DateTime.Parse` for conversion and calculates
    the difference in days.

---
+++ LeetCodeDailyQuestion/MiscellaneousProblems/P1360.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCodeDailyQuestion.MiscellaneousProblems
+{
+    internal class P1360
+    {
+        // 1360. Number of Days Between Two Dates
+        public int DaysBetweenDates(string date1, string date2)
+        {
+            DateTime dt1 = DateTime.Parse(date1), dt2 = DateTime.Parse(date2);
+            return Math.Abs((dt2 - dt1).Days);
+        }
+    }
+}
