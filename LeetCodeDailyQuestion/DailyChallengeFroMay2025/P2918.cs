e16503c794e9e655bc3c914d3a62abe2b5f6051e
YousryEssam <youssry.essam2111@gmail.com>
2025-05-10 20:11:43 +0300

    Add P2918 class with MinSum method for array sums
    Introduces a new class `P2918` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeFroMay2025`. The
    `MinSum` method calculates the minimum equal sum of two
    arrays after replacing zeros, handling various conditions
    to adjust the sums based on the number of zeros present
    in each array.

---
+++ LeetCodeDailyQuestion/DailyChallengeFroMay2025/P2918.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeFroMay2025
+{
+    internal class P2918
+    {
+        //2918. Minimum Equal Sum of Two Arrays After Replacing Zeros 
+        // 10-5-2025
+
+        public long MinSum(int[] nums1, int[] nums2)
+        {
+            int zeroCounter1 = 0, zeroCounter2 = 0;
+            long sumArr1 = 0, sumArr2 = 0;
+            foreach (int i in nums1)
+            {
+                if (i == 0) zeroCounter1++;
+                sumArr1 += i;
+            }
+            foreach (int i in nums2)
+            {
+                if (i == 0) zeroCounter2++;
+                sumArr2 += i;
+            }
+            if (zeroCounter1 > 1)
+            {
+                sumArr1 += (zeroCounter1 - 1);
+                zeroCounter1 = 1;
+            }
+            if (zeroCounter2 > 1)
+            {
+                sumArr2 += (zeroCounter2 - 1);
+                zeroCounter2 = 1;
+            }
+            if (zeroCounter1 == zeroCounter2 && zeroCounter1 == 1)
+            {
+                return Math.Max(sumArr1, sumArr2) + 1;
+            }
+            else if (zeroCounter1 != zeroCounter2)
+            {
+                if (zeroCounter1 == 1 && sumArr1 < sumArr2) return sumArr2;
+                else if (zeroCounter2 == 1 && sumArr1 > sumArr2) return sumArr1;
+            }
+            else if (sumArr1 == sumArr2)
+            {
+                return sumArr1;
+            }
+            return -1;
+        }
+    }
+}
