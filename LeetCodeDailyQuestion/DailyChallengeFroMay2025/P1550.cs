0a6ed3a6e3bbfdadc228b095b47c8cfe033c214f
YousryEssam <youssry.essam2111@gmail.com>
2025-05-11 14:09:52 +0300

    Add P1550 class with ThreeConsecutiveOdds method
    Introduces a new class `P1550` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeFroMay2025`.
    The `ThreeConsecutiveOdds` method checks for three
    consecutive odd numbers in an integer array, returning
    `true` if found, otherwise `false`. Includes necessary
    `using` directives for system libraries.

---
+++ LeetCodeDailyQuestion/DailyChallengeFroMay2025/P1550.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDailyQuestion.DailyChallengeFroMay2025
+{
+    internal class P1550
+    {
+        // 1550. Three Consecutive Odds
+        // 11-5-2025
+        public bool ThreeConsecutiveOdds(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 2; i++)
+            {
+                if (arr[i] % 2 == 1 && arr[i + 1] % 2 == 1 && arr[i + 2] % 2 == 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
