c4420b96b6fb2ecc25965f83016352edae99605e
YousryEssam <youssry.essam2111@gmail.com>
2025-05-15 11:32:45 +0300

    Add P2900 class with GetLongestSubsequence method
    Introduces a new class `P2900` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeFroMay2025`.
    The `GetLongestSubsequence` method returns the longest
    subsequence of words from the input array where no two
    adjacent words belong to the same group.

---
+++ LeetCodeDailyQuestion/DailyChallengeFroMay2025/P2900.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDailyQuestion.DailyChallengeFroMay2025
+{
+    internal class P2900
+    {
+        // 2900. Longest Unequal Adjacent Groups Subsequence I
+        public IList<string> GetLongestSubsequence(string[] words, int[] groups)
+        {
+            IList<string> result = new List<string>();
+            int n = words.Length;
+            int last = 1 - groups[0];
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0 || last != groups[i])
+                {
+                    result.Add(words[i]);
+                    last = groups[i];
+                }
+            }
+            return result;
+        }
+    }
+}
