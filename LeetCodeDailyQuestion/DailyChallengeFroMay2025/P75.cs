648c105146117875e4b6243ae3ea0467485e9c87
YousryEssam <youssry.essam2111@gmail.com>
2025-05-17 18:23:41 +0300

    Add P75 class with SortColors method for sorting colors
    Introduced a new class `P75` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeFroMay2025`.
    The `SortColors` method sorts an array of integers
    representing colors (0s, 1s, and 2s) by counting
    the occurrences of 0s and 1s, then populating
    the input array in sorted order. Added necessary
    using directives for system libraries.

---
+++ LeetCodeDailyQuestion/DailyChallengeFroMay2025/P75.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDailyQuestion.DailyChallengeFroMay2025
+{
+    internal class P75
+    {
+        // 75. Sort Colors 
+        // 17-5-2025
+        public void SortColors(int[] nums)
+        {
+            int cnt0 = 0, cnt1 = 0;
+            foreach (int i in nums) 
+            {
+                if(i==0) cnt0++;
+                else if(i==1) cnt1++;
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(cnt0 > 0)
+                {
+                    cnt0--;
+                    nums[i] = 0;
+                }
+                else if(cnt1 > 0)
+                {
+                    cnt1--;
+                    nums[i] = 1;
+                }
+                else
+                {
+                    nums[i] = 2;
+                }
+            }
+        }
+    }
+}
