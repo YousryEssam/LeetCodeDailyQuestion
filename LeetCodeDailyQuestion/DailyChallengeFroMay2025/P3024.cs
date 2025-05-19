49225d625a8efc557bd0cad9dbcbe55d36c768b8
YousryEssam <youssry.essam2111@gmail.com>
2025-05-19 10:01:16 +0300

    Add P3024 class for triangle type determination
    Introduces a new class `P3024` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeFroMay2025`.
    This class features a method `IsTriangle` to verify
    if three sides can form a triangle using the triangle
    inequality theorem. It also includes a public method
    `TriangleType` that classifies the triangle as none,
    equilateral, isosceles, or scalene based on the
    provided side lengths.

---
+++ LeetCodeDailyQuestion/DailyChallengeFroMay2025/P3024.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeFroMay2025
+{
+    internal class P3024
+    {
+        // 3024. Type of Triangle
+        // 19-5-2025
+        private bool IsTriangle(int[] nums)
+        {
+            int val1 = nums[0] + nums[1];
+            int val2 = nums[1] + nums[2];
+            int val3 = nums[2] + nums[0];
+            return val1 > nums[2] && val2 > nums[0] && val3 > nums[1];
+        }
+        public string TriangleType(int[] nums)
+        {
+            if (!IsTriangle(nums)) return "none";
+            if (nums[0] == nums[1] && nums[1] == nums[2]) return "equilateral";
+            if (nums[0] == nums[1] || nums[0] == nums[2] || nums[1] == nums[2]) return "isosceles";
+            return "scalene";
+        }
+    }
+}
