3ba7b87d4665c23e0faaa9625443f9e1440b1b68
YousryEssam <youssry.essam2111@gmail.com>
2025-06-01 21:38:17 +0300

    Update project settings and add P2929 class for candy distribution
    Modified `LeetCodeDailyQuestion.csproj` to remove the folder reference for "DailyChallengeForJune2025", set the target framework to `net8.0`, and enable implicit usings and nullable reference types.
    
    Added a new class `P2929` in `P2929.cs` under the namespace `LeetCodeDailyQuestion.DailyChallengeForJune2025`, which includes the method `DistributeCandies` to calculate the distribution of candies among children based on the parameters `n` and `limit`.

---
+++ LeetCodeDailyQuestion/DailyChallengeForJune2025/P2929.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForJune2025
+{
+    internal class P2929
+    {
+
+        // 01-06-2025
+        // 2929. Distribute Candies Among Children II
+        public long DistributeCandies(int n, int limit)
+        {
+            long answer = 0;
+            if (n > limit * 3)
+            {
+                return answer;
+            }
+            for (int i = 0; i <= Math.Min( n , limit); i++)
+            {
+                long remin = n - i;
+                if (remin <= limit * 2)
+                {
+                    long minVlaue = 0;
+                    long maxVlaue = remin;
+                    if (maxVlaue > limit)
+                    {
+                        minVlaue = maxVlaue - limit;
+                        maxVlaue = limit;
+                    }
+                    answer += (maxVlaue - minVlaue + 1);
+                }
+            }
+            return answer;
+        }
+    }
+}
