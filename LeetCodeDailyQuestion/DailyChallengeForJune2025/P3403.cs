a89ff47924534e7f5e5caaf8faa38a87e230fa42
YousryEssam <youssry.essam2111@gmail.com>
2025-06-04 21:34:56 +0300

    Add P3403 class with lexicographically largest string method
    Introduces a new class `P3403` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeForJune2025`.
    The class features the method `AnswerString`, which takes
    a string `word` and an integer `numFriends` to compute
    the lexicographically largest string possible under
    specified constraints. The implementation utilizes a
    nested loop and `StringBuilder` for efficient string
    construction and returns the appropriately truncated
    result.

---
+++ LeetCodeDailyQuestion/DailyChallengeForJune2025/P3403.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDailyQuestion.DailyChallengeForJune2025
+{
+    internal class P3403
+    {
+        // 04-06-2025
+        // 3403. Find the Lexicographically Largest String From the Box I
+        public string AnswerString(string word, int numFriends)
+        {
+            if (numFriends == 1) { return word; }
+            int resultLength = 0;
+            StringBuilder stringBuilder = new StringBuilder(word.Length);
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (resultLength == 0 || word[i] >= stringBuilder[0])
+                {
+                    int curLen = 0;
+                    bool isLexicographicallyLarger = false;
+                    for (int j = i; j < Math.Min(i + word.Length + 1 - numFriends, word.Length); j++)
+                    {
+                        if (curLen >= resultLength || word[j] > stringBuilder[curLen])
+                        {
+                            isLexicographicallyLarger = true;
+                        }
+                        else if (word[j] < stringBuilder[curLen] && !isLexicographicallyLarger)
+                        {
+                            break;
+                        }
+
+                        if (curLen >= stringBuilder.Length)
+                        {
+                            stringBuilder.Append(word[j]);
+                        }
+                        else
+                        {
+                            stringBuilder[curLen] = word[j];
+                        }
+                        curLen++;
+                    }
+                    if (isLexicographicallyLarger)
+                    {
+                        resultLength = curLen;
+                    }
+                }
+            }
+            return stringBuilder.ToString().Substring(0, resultLength);
+        }
+    }
+}
