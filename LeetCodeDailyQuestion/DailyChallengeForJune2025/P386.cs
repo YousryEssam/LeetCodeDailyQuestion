8b216c0c92b5b88fb6ebbff04f3eec9e2d6429c0
YousryEssam <youssry.essam2111@gmail.com>
2025-06-08 14:34:55 +0300

    Add P386 class with LexicalOrder method
    Introduces a new class `P386` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeForJune2025`.
    The `LexicalOrder` method generates a list of integers
    from 1 to `n` in lexicographical order using a loop
    and specific conditions to manipulate the current number.

---
+++ LeetCodeDailyQuestion/DailyChallengeForJune2025/P386.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForJune2025
+{
+    internal class P386
+    {
+        // 08-06-2025
+        // 386. Lexicographical Numbers
+        public IList<int> LexicalOrder(int n)
+        {
+            List<int> result = new List<int>();
+            int curr = 1;
+            for (int i = 0; i < n; i++)
+            {
+                result.Add(curr);
+                if (curr * 10 <= n)
+                {
+                    curr *= 10;
+                }
+                else
+                {
+                    while (curr % 10 == 9 || curr >= n)
+                    {
+                        curr /= 10;
+                    }
+                    curr++;
+                }
+            }
+            return result;
+        }
+    }
+}
