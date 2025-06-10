53f7638a6a18b4bf014c4d7312a0699ec0d84f67
YousryEssam <youssry.essam2111@gmail.com>
2025-06-10 16:04:37 +0300

    Add P3342 class with MaxDifference method
    This commit introduces a new class `P3342` in the namespace `LeetCodeDailyQuestion.DailyChallengeForJune2025`. The class features the `MaxDifference` method, which computes the maximum difference between the highest frequencies of odd and even character counts in a given string `s`. The implementation includes frequency counting and logic to determine the required difference.

---
+++ LeetCodeDailyQuestion/DailyChallengeForJune2025/P3342.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForJune2025
+{
+    internal class P3342
+    {
+        // 10-06-2025
+        // 3442. Maximum Difference Between Even and Odd Frequency I
+        public int MaxDifference(string s)
+        {
+            int oddCnt = 0;
+            int evenCnt = 0;
+
+            int[] frq = new int[26];
+            foreach (var i in s) frq[i - 'a']++;
+
+            for (int i = 0; i < 26; i++)
+            {
+                if (frq[i] % 2 == 0)
+                {
+                    if (evenCnt == 0) evenCnt = frq[i];
+                    if (frq[i] < evenCnt && frq[i] != 0) evenCnt = frq[i];
+                }
+                else
+                {
+                    if (frq[i] > oddCnt) oddCnt = frq[i];
+                }
+            }
+
+            return oddCnt - evenCnt;
+        }
+    }
+}
