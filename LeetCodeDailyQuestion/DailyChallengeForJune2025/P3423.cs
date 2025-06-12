3cc5b0618b65caaf728e60c704fe1130d9f7691d
YousryEssam <youssry.essam2111@gmail.com>
2025-06-12 21:06:40 +0300

    Add P3423 class for maximum adjacent distance calculation
    Introduces a new class `P3423` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeForJune2025`.
    The class features the method `MaxAdjacentDistance`,
    which computes the maximum difference between adjacent
    elements in a circular array, including the difference
    between the first and last elements.

---
+++ LeetCodeDailyQuestion/DailyChallengeForJune2025/P3423.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForJune2025
+{
+    internal class P3423
+    {
+        // 12-06-2025
+        // 3423. Maximum Difference Between Adjacent Elements in a Circular Array 
+        public int MaxAdjacentDistance(int[] nums)
+        {
+            int answer = 0;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                int val1 = nums[i];
+                int val2 = nums[i + 1];
+                if (val1 >= val2)
+                {
+                    if(answer <= val1 - val2)
+                    {
+                        answer = val1 - val2;
+                    }
+                }
+                else
+                {
+                    if(answer <= val2 - val1)
+                    {
+                        answer = val2 - val1;
+                    }
+                }
+            }
+
+            return Math.Max(answer, Math.Abs(nums[0] - nums[nums.Length-1]));
+        }
+    }
+}
