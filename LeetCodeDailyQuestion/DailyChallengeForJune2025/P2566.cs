cf616248382d427677b39b08a5b821177cae749c
YousryEssam <youssry.essam2111@gmail.com>
2025-06-14 19:44:57 +0300

    Add P2566 class with MinMaxDifference method
    Introduces a new class `P2566` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeForJune2025`.
    The `MinMaxDifference` method computes the maximum
    difference by remapping a digit in the input integer `num`,
    replacing the first non-'9' digit with '9' for the maximum
    value and the first digit with '0' for the minimum value,
    returning their difference.

---
+++ LeetCodeDailyQuestion/DailyChallengeForJune2025/P2566.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForJune2025
+{
+    internal class P2566
+    {
+
+        // 14-06-2025
+        // 2566. Maximum Difference by Remapping a Digit 
+        public int MinMaxDifference(int num)
+        {
+            int maxValue = 0;
+            int minValue = num;
+            string maxStr = num.ToString();
+            string minStr = num.ToString();
+
+            int idx = 0;
+            for (int i = 0; i < maxStr.Length; i++)
+            {
+                if (maxStr[i] < '9')
+                {
+                    idx = i;
+                    break;
+                }
+            }
+            minStr = minStr.Replace(minStr[0], '0');
+            maxStr = maxStr.Replace(maxStr[idx], '9');
+            maxValue = int.Parse(maxStr);
+            minValue = int.Parse(minStr);
+            return maxValue - minValue;
+        }
+    }
+}
