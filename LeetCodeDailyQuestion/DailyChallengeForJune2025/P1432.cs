8f14d6151d03a885d745aaf4063f53554c2161d2
YousryEssam <youssry.essam2111@gmail.com>
2025-06-15 22:50:48 +0300

    Add P1432 class with MaxDiff method for integer changes
    Introduces a new class `P1432` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeForJune2025`.
    The `MaxDiff` method calculates the maximum difference
    obtainable by changing an integer by modifying its
    string representation to maximize and minimize its value.

---
+++ LeetCodeDailyQuestion/DailyChallengeForJune2025/P1432.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForJune2025
+{
+    internal class P1432
+    {
+        // 15-06-2025
+        // 1432. Max Difference You Can Get From Changing an Integer 
+        public int MaxDiff(int num)
+        {
+            int maxValue = 0;
+            int minValue = num;
+            string maxStr = num.ToString();
+            string minStr = num.ToString();
+
+            int idxMax = 0;
+            for (int i = 0; i < maxStr.Length; i++)
+            {
+                if (maxStr[i] < '9')
+                {
+                    idxMax = i;
+                    break;
+                }
+            }
+            maxStr = maxStr.Replace(maxStr[idxMax], '9');
+            if (minStr[0] != '1')
+            {
+                minStr = minStr.Replace(minStr[0], '1');
+            }
+            else
+            {
+                int idxMin = 0;
+                for (int i = 1; i < minStr.Length; i++)
+                {
+                    if (minStr[i] > '0' && minStr[i] != '1')
+                    {
+                        idxMin = i;
+                        break;
+                    }
+                }
+                if(idxMin != 0 )
+                   minStr = minStr.Replace(minStr[idxMin], '0');
+            }
+            maxValue = int.Parse(maxStr);
+            minValue = int.Parse(minStr);
+            return maxValue - minValue;
+        }
+    }
+}
