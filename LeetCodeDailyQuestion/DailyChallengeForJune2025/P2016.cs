89c1650f44f6b55e37e83f4cbee7edb80fc7cf3b
YousryEssam <youssry.essam2111@gmail.com>
2025-06-16 23:01:01 +0300

    Add P2016 class with MaximumDifference method
    Introduces a new class `P2016` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeForJune2025`.
    The `MaximumDifference` method calculates the maximum
    difference between increasing elements in an integer
    array using a suffix array for efficient computation.
    Returns -1 if no valid difference exists.

---
+++ LeetCodeDailyQuestion/DailyChallengeForJune2025/P2016.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForJune2025
+{
+    internal class P2016
+    {
+        // 16-06-2025
+        // 2016. Maximum Difference Between Increasing Elements 
+        public int MaximumDifference(int[] nums)
+        {
+            int[] suffixMax = new int[nums.Length];
+            for(int i = nums.Length - 1; i >= 0; i--)
+            {
+                if (i == nums.Length - 1)
+                {
+                    suffixMax[i] = nums[i];
+                }
+                else
+                {
+                    suffixMax[i] = Math.Max(suffixMax[i + 1], nums[i]);
+                }
+            }
+            int maxDiff = -1;
+            for(int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] < suffixMax[i + 1])
+                {
+                    maxDiff = Math.Max(maxDiff, suffixMax[i + 1] - nums[i]);
+                }
+            }
+            return maxDiff;
+        }
+    }
+}
