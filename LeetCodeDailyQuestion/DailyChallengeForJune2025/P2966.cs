2bd184ea33df366f7fb580ea8953755160118656
YousryEssam <youssry.essam2111@gmail.com>
2025-06-18 16:08:22 +0300

    Add P2966 class for dividing arrays with constraints
    Introduces the `P2966` class in the namespace
    `LeetCodeDailyQuestion.DailyChallengeForJune2025`.
    The class features the `DivideArray` method, which splits
    an input array `nums` into smaller arrays of size 3,
    ensuring the maximum difference between the first and
    last elements does not exceed a specified value `k`.
    A validation method `ValidArray` checks this condition
    before performing the division. Additional using directives
    and comments have been added for clarity.

---
+++ LeetCodeDailyQuestion/DailyChallengeForJune2025/P2966.cs
@@ -0,0 +1,44 @@
+﻿using System.Formats.Asn1;
+
+namespace LeetCodeDailyQuestion.DailyChallengeForJune2025
+{
+    internal class P2966
+    {
+        // 18-06-2025
+        // By Yousry Essam
+        // 2966. Divide Array Into Arrays With Max Difference
+        private bool ValidArray(int[] nums, int numOfArrays, int difference)
+        {
+            for (int i = 0; i < numOfArrays; i++)
+            {
+                int firstIdx = i * 3;
+                int lastIdx = (i + 1) * 3 - 1;
+                if (nums[lastIdx] - nums[firstIdx] > difference)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public int[][] DivideArray(int[] nums, int k)
+        {
+            int numOfArrays = nums.Length / 3;
+            Array.Sort(nums);
+            if (!ValidArray(nums, numOfArrays, k))
+            {
+                return new int[0][];
+            }
+            int idx = 0;
+            int[][] result = new int[numOfArrays][];
+            for (int i = 0; i < numOfArrays; i++)
+            {
+                result[i] = new int[3];
+                for(int j = 0; j < 3; j ++)
+                {
+                    result[i][j] = nums[idx++];
+                }
+            }
+            return result;
+        }
+    }
+}
