556ce9bcaed3b5e150c690b6795cfe168c5552ff
YousryEssam <youssry.essam2111@gmail.com>
2025-06-19 22:00:53 +0300

    Add P2294 class for partitioning array by max difference
    Introduces a new class `P2294` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeForJune2025`.
    The class features a method `PartitionArray` that sorts
    an integer array and counts the number of partitions
    where the maximum difference between elements does not
    exceed a given value `k`. The method efficiently
    increments the partition count based on the difference
    between elements.

---
+++ LeetCodeDailyQuestion/DailyChallengeForJune2025/P2294.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForJune2025
+{
+    internal class P2294
+    {
+        // 19-06-2025
+        // By Yousry Essam
+        // 2294. Partition Array Such That Maximum Difference Is K
+        public int PartitionArray(int[] nums, int k)
+        {
+            int result = 1;
+            Array.Sort(nums);
+            int startRange = 0, n = nums.Length; 
+            for(int i = 0; i < n; i ++ )
+            {
+                if (nums[i] - nums[startRange] > k )
+                {
+                    result++;
+                    startRange = i;
+                }
+            }
+            return result;
+        }
+    }
+}
