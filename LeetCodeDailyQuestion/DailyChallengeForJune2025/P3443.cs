8ade7db6412448d6e7a01bf86b8dde871ed7e525
YousryEssam <youssry.essam2111@gmail.com>
2025-06-20 20:56:49 +0300

    Add P3443 class for maximum Manhattan distance calculation
    Introduces a new class `P3443` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeForJune2025`. This class
    provides methods to compute the maximum Manhattan distance
    after a specified number of directional changes. Key methods
    include `ManhattanDistance` for calculating distance between
    points and `BounsMoves` for determining bonus moves based
    on available directions. The `MaxDistance` method processes
    a string of directions to find the maximum achievable distance.

---
+++ LeetCodeDailyQuestion/DailyChallengeForJune2025/P3443.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForJune2025
+{
+    internal class P3443
+    {
+        // 20-06-2025
+        // By Yousry Essam
+        // 3443. Maximum Manhattan Distance After K Changes
+        private int ManhattanDistance(int x1, int x2, int y1, int y2)
+        {
+            return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+        }
+        private int BounsMoves(int left, int right, int up, int down, int k)
+        {
+            int bouns = 0;
+            if (left > 0 && right > 0)
+            {
+                int temp = Math.Min(left, right);
+                temp = Math.Min(temp, k);
+                bouns += temp;
+                k -= temp;
+            }
+            if (up > 0 && down > 0)
+            {
+                int temp = Math.Min(up, down);
+                temp = Math.Min(temp, k);
+                bouns += temp;
+                k -= temp;
+            }
+            return 2 * bouns;
+        }
+        public int MaxDistance(string s, int k)
+        {
+            int maxDistance = 0;
+            int left = 0, right = 0, up = 0, down = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'N') up++;
+                else if (s[i] == 'S') down++;
+                else if (s[i] == 'E') right++;
+                else left++;
+                int calcValue = ManhattanDistance(left, right, up, down);
+                calcValue += BounsMoves(left, right, up, down, k);
+
+                maxDistance = Math.Max(maxDistance, calcValue);
+            }
+
+            return maxDistance;
+        }
+    }
+}
