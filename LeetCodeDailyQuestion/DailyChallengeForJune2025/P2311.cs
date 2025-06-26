1a002b872dbef7bfde16d2ea698897e372308572
YousryEssam <youssry.essam2111@gmail.com>
2025-06-26 19:50:45 +0300

    Add P2311 class with LongestSubsequence method
    Introduces a new class `P2311` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeForJune2025`.
    The class features the `LongestSubsequence` method,
    which computes the length of the longest binary
    subsequence of a string `s` that is less than or
    equal to a given integer `k`, iterating through
    the string in reverse and adjusting `k` accordingly.

---
+++ LeetCodeDailyQuestion/DailyChallengeForJune2025/P2311.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForJune2025
+{
+    internal class P2311
+    {
+        // 26-06-2025
+        // By Yousry Essam
+        // 2311. Longest Binary Subsequence Less Than or Equal to K
+
+        public int LongestSubsequence(string s, int k)
+        {
+            int n = 0;
+            for (int i = s.Length; i-- > 0;)
+            {
+                if (k > 0)
+                {
+                    k -= s[i] == '1' ? 1 : 0;
+                    k >>= 1;
+                    ++n;
+                }
+                else if (s[i] == '0') ++n;
+            }
+            return n;
+        }
+    }
+}
