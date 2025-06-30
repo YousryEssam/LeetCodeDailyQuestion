231bae8debc3ba04496959c1067f273779f638e7
YousryEssam <youssry.essam2111@gmail.com>
2025-06-30 04:42:29 +0300

    Add P594 class for longest harmonious subsequence
    Introduces a new class `P594` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeForJune2025`.
    The class features the method `FindLHS`, which computes
    the length of the longest harmonious subsequence in an
    array of integers using sorting and a two-pointer technique.
    Comments are included to indicate the date and author.

---
+++ LeetCodeDailyQuestion/DailyChallengeForJune2025/P594.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForJune2025
+{
+    internal class P594
+    {
+        // 30-06-2025
+        // By Yousry Essam
+        // 594. Longest Harmonious Subsequence
+        public int FindLHS(int[] nums)
+        {
+            Array.Sort(nums);
+            int LHS = 0;
+            int l = 0, r = 0;
+            while (r < nums.Length)
+            {
+                if (nums[r] - nums[l] == 0)
+                {
+                    r++;
+                }
+                else if (nums[r] - nums[l] == 1)
+                {
+                    LHS = Math.Max(LHS, r - l + 1);
+                    r++;
+                }
+                else
+                {
+                    l++;
+                }
+            }
+            return LHS;
+        }
+    }
+}
