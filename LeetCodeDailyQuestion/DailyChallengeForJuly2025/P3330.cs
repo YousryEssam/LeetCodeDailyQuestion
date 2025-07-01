35312357f67a33c8b4d7694f135c64a47423d3ca
YousryEssam <youssry.essam2111@gmail.com>
2025-07-01 18:46:39 +0300

    Add P3330 class with PossibleStringCount method
    Introduces a new class `P3330` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeForJuly2025`.
    The `PossibleStringCount` method calculates the number
    of possible original typed strings from the input string
    `word` by counting consecutive identical characters.
    Comments are included to indicate the date and author.

---
+++ LeetCodeDailyQuestion/DailyChallengeForJuly2025/P3330.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForJuly2025
+{
+    internal class P3330
+    {
+        // 01-07-2025
+        // By Yousry Essam
+        // 3330. Find the Original Typed String I
+        public int PossibleStringCount(string word)
+        {
+            int answer = 1;
+            int temp = 0; 
+            for (int i = 0; i < word.Length - 1; i++)
+            {
+                if (word[i] == word[i + 1])
+                {
+                    temp++;
+                }
+                else
+                {
+                    answer += (temp - 1);
+                    temp = 1;
+                }
+            }
+            return answer + temp - 1;
+        }
+    }
+}
