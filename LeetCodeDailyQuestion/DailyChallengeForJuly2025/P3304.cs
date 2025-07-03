82837901362c15f83b0693c861b07c922dd06168
YousryEssam <youssry.essam2111@gmail.com>
2025-07-03 21:56:33 +0300

    Add P3304 class for K-th character calculation
    Introduces a new class `P3304` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeForJuly2025`.
    The class features a method `KthCharacter` that computes
    and returns the k-th character in a string game based
    on the input integer `k`, utilizing bit manipulation
    and logarithmic calculations.

---
+++ LeetCodeDailyQuestion/DailyChallengeForJuly2025/P3304.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForJuly2025
+{
+    // 03-07-2025
+    // By Yousry Essam
+    // 3304. Find the K-th Character in String Game I
+    internal class P3304
+    {
+        public char KthCharacter(int k)
+        {
+            int ans = 0;
+            int t;
+            while (k != 1)
+            {
+                t = (int)Math.Log(k, 2);
+                if ((1 << t) == k)
+                {
+                    t--;
+                }
+                k = k - (1 << t);
+                ans++;
+            }
+            return (char)('a' + ans);
+        }
+    }
+}
