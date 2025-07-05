373dc2e14f0fae07c30732203ff55078d96aa0e6
YousryEssam <youssry.essam2111@gmail.com>
2025-07-05 20:27:01 +0300

    Add P1394 class with FindLucky method implementation
    Introduced a new class `P1394` in the namespace
    `LeetCodeDailyQuestion.DailyChallengeForJuly2025`.
    The `FindLucky` method identifies the largest lucky integer
    in an array, where a lucky integer appears in the array
    exactly as many times as its value. The implementation
    utilizes a dictionary to track integer frequencies and
    includes comments with the date and author information.

---
+++ LeetCodeDailyQuestion/DailyChallengeForJuly2025/P1394.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForJuly2025
+{
+    internal class P1394
+    {
+        // 05-07-2025
+        // By Yousry Essam
+        // 1394. Find Lucky Integer in an Array
+        public int FindLucky(int[] arr)
+        {
+            int largestLuckyInteger = -1;
+            Dictionary<int, int> frq = new Dictionary<int, int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (frq.ContainsKey(arr[i]))
+                {
+                    frq[arr[i]]++;
+                }
+                else
+                {
+                    frq.Add(arr[i], 1);
+                }
+            }
+            foreach (var i in frq)
+            {
+                if (i.Value == i.Key)
+                {
+                    largestLuckyInteger = Math.Max(largestLuckyInteger, i.Value);
+                }
+            }
+
+            return largestLuckyInteger;
+        }
+    }
+}
