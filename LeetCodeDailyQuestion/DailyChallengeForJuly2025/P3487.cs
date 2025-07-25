1d34956ef381618bb9b14363b30c382890a65664
YousryEssam <youssry.essam2111@gmail.com>
2025-07-25 22:29:55 +0300

    3487. Maximum Unique Subarray Sum After Deletion 25-07-2025

---
+++ LeetCodeDailyQuestion/DailyChallengeForJuly2025/P3487.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForJuly2025
+{
+    internal class P3487
+    {
+        // 25-07-2025
+        // By Yousry Essam
+        // 3487. Maximum Unique Subarray Sum After Deletion
+        public int MaxSum(int[] nums)
+        {
+            int maxSum = 0;
+            bool notEmpty = false;
+            Array.Sort(nums);
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i - 1] && nums[i] >= 0)
+                {
+                    maxSum += nums[i];
+                    if (!notEmpty && nums[i - 1] >= 0)
+                    {
+                        maxSum += nums[i - 1];
+                    }
+                    notEmpty = true;
+                }
+            }
+
+
+            return notEmpty ? maxSum : nums[nums.Length - 1];
+        }
+    }
+}
