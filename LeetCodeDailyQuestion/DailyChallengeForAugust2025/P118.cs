a16424cea791779024d79909c7953a5bc2cefb61
YousryEssam <youssry.essam2111@gmail.com>
2025-08-01 20:22:32 +0300

    118. Pascal's Triangle

---
+++ LeetCodeDailyQuestion/DailyChallengeForAugust2025/P118.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForAugust2025
+{
+    internal class P118
+    {
+        // 01-08-2025
+        // By Yousry Essam
+        // 118. Pascal's Triangle
+        public IList<IList<int>> Generate(int numRows)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            for (int i = 0; i < numRows; i++)
+            {
+                IList<int> row = new List<int>();
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j == 0 || j == i)
+                    {
+                        row.Add(1);
+                    }
+                    else
+                    {
+                        int value = result[i - 1][j - 1] + result[i - 1][j];
+                        row.Add(value);
+                    }
+                }
+                result.Add(row);
+            }
+            return result;
+        }
+    }
+}
