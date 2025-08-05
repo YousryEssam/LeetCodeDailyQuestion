fc4b5f665952b1d465103e6eeed28600b1cdf514
YousryEssam <youssry.essam2111@gmail.com>
2025-08-05 22:56:42 +0300

    3477. Fruits Into Baskets II

---
+++ LeetCodeDailyQuestion/DailyChallengeForAugust2025/P3477.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForAugust2025
+{
+    internal class P3477
+    {
+        // 3477. Fruits Into Baskets II
+        public int NumOfUnplacedFruits(int[] fruits, int[] baskets)
+        {
+            int unplacedCount = 0;
+            for(int i = 0; i < fruits.Length; i++)
+            {
+                bool placed = false;
+                for (int  j = 0; j < baskets.Length; j++)
+                {
+                    if (baskets[j] >= fruits[i] && baskets[j] != -1)
+                    {
+                        baskets[j] = -1;
+                        placed = true;
+                        break;
+                    }
+                }
+                if(!placed)
+                {
+                    unplacedCount++;
+                }
+            }
+            return unplacedCount;
+        }
+    }
+}
