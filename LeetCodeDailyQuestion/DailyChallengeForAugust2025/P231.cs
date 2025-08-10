1e66dd08d3bd8da70d9a70e1ef37f5b682ef71bf
YousryEssam <youssry.essam2111@gmail.com>
2025-08-10 20:46:38 +0300

    231. Power of Two

---
+++ LeetCodeDailyQuestion/DailyChallengeForAugust2025/P231.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForAugust2025
+{
+    internal class P231
+    {
+        // 231. Power of Two
+        public bool IsPowerOfTwo(int n)
+        {
+            if (n == 0) return false;
+            while (n != 1)
+            {
+                if (n % 2 == 0)
+                    n /= 2;
+                else
+                    break;
+            }
+
+            return n == 1;
+        }
+    }
+}
