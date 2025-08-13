1dbc84b4a316888c260de175f1038707c27a9733
YousryEssam <youssry.essam2111@gmail.com>
2025-08-13 21:36:58 +0300

    326. Power of Three

---
+++ LeetCodeDailyQuestion/DailyChallengeForAugust2025/P326.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForAugust2025
+{
+    internal class P326
+    {
+        // 326. Power of Three
+        public bool IsPowerOfThree(int n)
+        {
+            if (n == 0) return false;
+            while (n != 1)
+            {
+                if (n % 3 != 0) return false;
+                n /= 3;
+            }
+            return n == 1;
+        }
+    }
+}
