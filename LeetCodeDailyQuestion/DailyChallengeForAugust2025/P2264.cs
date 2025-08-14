9e14a2599623b7b9ead31644b8c81ee8866a248c
YousryEssam <youssry.essam2111@gmail.com>
2025-08-14 21:10:53 +0300

    2264. Largest 3-Same-Digit Number in String

---
+++ LeetCodeDailyQuestion/DailyChallengeForAugust2025/P2264.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForAugust2025
+{
+    internal class P2264
+    {
+        // 2264. Largest 3-Same-Digit Number in String\
+        public string LargestGoodInteger(string num)
+        {
+            char maxDigit = '-';
+            for(int i  =  0;  i < num.Length - 2; i++)
+            {
+                if (num[i] == num[i + 1] && num[i] == num[i + 2])
+                {
+                    if (num[i] > maxDigit)
+                    {
+                        maxDigit = num[i];
+                        i+= 2; // Skip the next two characters since they are the same
+                        if (maxDigit == '9') // Early exit if we found the largest digit
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            return maxDigit == '-' ? string.Empty : new string(maxDigit, 3);
+        }
+    }
+}
