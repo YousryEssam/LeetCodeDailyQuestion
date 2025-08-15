1ad433cd398985c1dee6eb36268d6d9f75a7c7ce
YousryEssam <youssry.essam2111@gmail.com>
2025-08-15 23:54:38 +0300

    342. Power of Four

---
+++ LeetCodeDailyQuestion/DailyChallengeForAugust2025/P342.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForAugust2025
+{
+    internal class P342
+    {
+        // 342. Power of Four
+        public bool IsPowerOfFour(int n)
+        {
+            if (n <= 0) return false;
+            while (n % 4 == 0)
+            {
+                n /= 4;
+            }
+            return n == 1;
+        }
+    }
+}
