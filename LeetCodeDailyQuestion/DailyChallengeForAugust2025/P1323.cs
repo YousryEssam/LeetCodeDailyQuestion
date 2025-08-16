8de108fab8e997a61c605aed480c8ca15e413c65
YousryEssam <youssry.essam2111@gmail.com>
2025-08-17 01:41:18 +0300

    1323. Maximum 69 Number

---
+++ LeetCodeDailyQuestion/DailyChallengeForAugust2025/P1323.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForAugust2025
+{
+    internal class P1323
+    {
+        // 1323. Maximum 69 Number
+        public int Maximum69Number(int num)
+        {
+            char[] numArray = num.ToString().ToCharArray();
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                if (numArray[i] == '6')
+                {
+                    numArray[i] = '9';
+                    return int.Parse(new string(numArray));
+                }
+            }
+            return num;
+        }
+    }
+}
