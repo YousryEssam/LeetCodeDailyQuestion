0b715d12b22cbd8a714ae52864a8e1c4ffeb2bb1
YousryEssam <youssry.essam2111@gmail.com>
2025-08-20 02:41:58 +0300

    2348. Number of Zero-Filled Subarrays

---
+++ LeetCodeDailyQuestion/DailyChallengeForAugust2025/P2348.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForAugust2025
+{
+    internal class P2348
+    {
+        // 2348. Number of Zero-Filled Subarrays
+        public long ZeroFilledSubarray(int[] nums)
+        {
+            long count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                long tempCnt = 0;
+                while (i < nums.Length && nums[i] == 0)
+                {
+                    tempCnt++;
+                    i++;
+                }
+                count += ((tempCnt * (tempCnt + 1)) / 2);
+            }
+            return count;
+        }
+    }
+}
