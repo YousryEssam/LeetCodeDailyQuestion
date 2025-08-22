ea463bea974135c04aced10682e7eef175a79fbb
YousryEssam <youssry.essam2111@gmail.com>
2025-08-22 19:21:47 +0300

    3195. Find the Minimum Area to Cover All Ones I

---
+++ LeetCodeDailyQuestion/DailyChallengeForAugust2025/P3195.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeDailyQuestion.DailyChallengeForAugust2025
+{
+    internal class P3195
+    {
+        // 3195. Find the Minimum Area to Cover All Ones I
+        public int MinimumArea(int[][] grid)
+        {
+            int minX = int.MaxValue, maxX = int.MinValue;
+            int minY = int.MaxValue, maxY = int.MinValue;
+            for(int i  = 0; i < grid.Length; i++)
+            {
+                for(int j = 0; j < grid[i].Length; j++)
+                {
+                    if(grid[i][j] == 1)
+                    {
+                        minX = Math.Min(minX, i);
+                        maxX = Math.Max(maxX, i);
+                        minY = Math.Min(minY, j);
+                        maxY = Math.Max(maxY, j);
+                    }
+                }
+            }
+            return (maxX - minX + 1) * (maxY - minY + 1);
+        }
+    }
+}
