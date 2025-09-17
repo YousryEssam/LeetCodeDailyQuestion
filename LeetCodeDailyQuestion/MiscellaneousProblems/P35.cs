d3debdf93200f837de1df96ca3e53b300b2e68d5
YousryEssam <youssry.essam2111@gmail.com>
2025-09-17 19:13:41 +0300

    35. Search Insert Position

---
+++ LeetCodeDailyQuestion/MiscellaneousProblems/P35.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCodeDailyQuestion.MiscellaneousProblems
+{
+    internal class P35
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            int idx = 0;
+            int left = 0, right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                //int mid = left + (right - left) / 2;
+                int mid = (left + right) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if (nums[mid] <= target)
+                {
+                    left = mid + 1;
+                    idx = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                    idx = mid;
+                }
+            }
+            return idx;
+        }
+    }
+}
