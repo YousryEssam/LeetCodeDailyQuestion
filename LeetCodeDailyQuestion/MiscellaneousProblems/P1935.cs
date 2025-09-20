000d9fbddb6f8f73345602a2c3265222785604e6
YousryEssam <youssry.essam2111@gmail.com>
2025-09-20 22:05:08 +0300

    1935. Maximum Number of Words You Can Type

---
+++ LeetCodeDailyQuestion/MiscellaneousProblems/P1935.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCodeDailyQuestion.MiscellaneousProblems
+{
+    internal class P1935
+    {
+        // 1935. Maximum Number of Words You Can Type
+        // Beats 100.00% 
+        public int CanBeTypedWords(string text, string brokenLetters)
+        {
+            int MaxNumberOfWords = 0;
+            bool[] BadKeys = new bool[26];
+            for (int i = 0; i < brokenLetters.Length; i++)
+            {
+                BadKeys[brokenLetters[i] - 'a'] = true;
+            }
+            bool hasBadKey = false, sameWord = false;
+            foreach(var i in text)
+            {
+                if(i == ' ')
+                {
+                    if(sameWord)
+                    {
+                        if(!hasBadKey)
+                        {
+                            MaxNumberOfWords++;
+                        }
+                    }
+                    sameWord = false;
+                    hasBadKey = false;
+                }
+                else
+                {
+                    sameWord = true;
+                    if (BadKeys[i-'a'])
+                    {
+                        hasBadKey = true;
+                    }
+                }
+            }
+            if (sameWord)
+            {
+                if (!hasBadKey)
+                {
+                    MaxNumberOfWords++;
+                }
+            }
+            return MaxNumberOfWords;
+        }
+    }
+}
